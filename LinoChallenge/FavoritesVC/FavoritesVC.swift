70d96e431b7cd89954bba7e9ef97de090e368250
Krakenbuild <krakenbuild@Mac-mini-de-Roberto.local>
2019-01-19 17:32:08 -0600

    adding group order

---
+++ LinoChallenge/FavoritesVC/FavoritesVC.swift
@@ -0,0 +1,30 @@
+//
+//  FavoritesVC.swift
+//  LinoChallenge
+//
+//  Created by Krakenbuild on 19/01/19.
+//  Copyright © 2019 Alberto Josue Gonzalez Juarez. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
