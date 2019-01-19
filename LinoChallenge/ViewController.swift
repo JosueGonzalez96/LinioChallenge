23690ade10da7d04cfbd807e31a48435bb665096
Krakenbuild <krakenbuild@mac-mini-de-roberto.local>
2019-01-19 16:03:32 -0600

    Initial Commit

---
+++ LinoChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LinoChallenge
+//
+//  Created by Krakenbuild on 19/01/19.
+//  Copyright © 2019 Alberto Josue Gonzalez Juarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
